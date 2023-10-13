f60983b5c091bbba87fcbe6b5ff33e7c4cb3e31e
HaoGus <HaoGus3729549.github.io>
2023-10-13 16:39:37 +0800

    fix description

--- src/main/java/com/haogus/backend/PostManager.java
+++ src/main/java/com/haogus/backend/PostManager.java
@@ -3,12 +3,7 @@ package com.haogus.backend;
 import com.haogus.dao.PostDao;
 import com.haogus.util.MysqlUtil;
 
-/**
- * @author guokang
- * @email guokang@tomonline-inc.com
- * @date 2023/10/8 9:39
- * @description
- */
+
 public class PostManager {
 
     public static PostDao postDao = MysqlUtil.getDao(PostDao.class);
